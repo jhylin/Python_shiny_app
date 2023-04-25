808898c43e2cdfd916b1bf90f3a9144c1fe9c449
Jenny HY Lin <jennyhylin@gmail.com>
2023-04-25 18:10:50 +1200

    Draft app in progress

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from shiny import App, render, ui
 
-app_ui = ui.page_fluid(
-    ui.h2("Hello Shiny!"),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
-)
+# User interface
+# Add inputs and outputs
 
+# Inputs
+app_ui = ui.input_select(
+    "Molecular properties", # ID
+    "Select a property:", # Label
+    {
+    "Partition coefficents",
+    "Complexitys",
+    "Molecular weights",
+    "Polar surface areas"
+    }
+),
+# Outputs
+ui.output_plot
 
+# Server
 def server(input, output, session):
     @output
     @render.text
     def txt():
         return f"n*2 is {input.n() * 2}"
 
-
+# Combine UI & server into Shiny app
 app = App(app_ui, server)
