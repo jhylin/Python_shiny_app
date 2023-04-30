42209ff5e429b441556a3e87a55df37cbfd79897
Jenny HY Lin <jennyhylin@gmail.com>
2023-04-30 12:49:38 +1200

    final draft version

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ app_ui = ui.page_fluid(
         # Place selection boxes & texts in same row
         ui.row(
             # Divide the row into two columns
-            # Column 1 - selection drop-down boxes
+            # Column 1 - selection drop-down boxes x 2
             ui.column(
                 4, ui.input_select(
                 # Specify x variable input
-                "x", label = "Select a molecular property for x axis:", 
+                "x", label = "x axis:", 
                 choices = ["Partition coefficients", 
                            "Complexity",
                            "Heavy atom count",
@@ -45,7 +45,7 @@ app_ui = ui.page_fluid(
                 ), 
                 ui.input_select(
                 # Specify y variable input
-                "y", label = "Select a molecular property for y axis:",
+                "y", label = "y axis:",
                 choices = ["Partition coefficients", 
                            "Complexity",
                            "Heavy atom count",
@@ -58,20 +58,35 @@ app_ui = ui.page_fluid(
                            "Total atom stereocenter count", 
                            "Total bond stereocenter count"]  
                 )),
-            # Column 2 - texts
+            # Column 2 - add texts regarding plots
             ui.column(
-                8, ui.p(
-                        """
-                        This is an example of using PubChem data on compounds used in COVID-19 clinical trials.
-                        Source of this dataset can be accessed at: https://pubchem.ncbi.nlm.nih.gov/#tab=compound&query=covid-19%20clinicaltrials.
-                        Part_coef_group means partition coefficient groups as shown in the legend on the right. By clicking on each
-                        category will remove the data points in the scatter plot. Clicking on the same category will return the data points back to the plot.
-                        Hover over each data point to see the compound name and relevant molecular properties."""
-                        )              
-                ),
-            ),
-        output_widget("my_widget") 
+            8,
+            ui.p("Select different molecular properties as x and y axes to produce a scatter plot."),
+            ui.tags.ul(
+                ui.tags.li(
+                    """
+                    Part_coef_group means groups of partition coefficient (xlogp) as shown in the legend on the right""" 
+                ), 
+                ui.tags.li(
+                    """
+                    Toggle each partition coefficient category by clicking on the group names"""
+                ), 
+                ui.tags.li(
+                    """
+                    Hover over each data point to see compound name and relevant molecular properties"""
+                )
+            )),
+        # Output as a widget (interactive plot)
+        output_widget("my_widget"), 
+        # Add texts for data source
+        ui.row(
+            ui.p(
+                """
+                Data on compounds used in COVID-19 clinical trials curated by PubChem - data access: https://pubchem.ncbi.nlm.nih.gov/#tab=compound&query=covid-19%20clinicaltrials.""" 
+            )         
+        ) 
     )
+)
 
 
 # Server---
@@ -81,7 +96,7 @@ def server(input, output, session):
     @render_widget
     def my_widget():
         fig = px.scatter(
-            df, x = input.x(), y = input.y(), 
+            df, x = input.x(), y = input.y(),
             color = "Part_coef_group", 
             hover_name = "Compound name"
         )
